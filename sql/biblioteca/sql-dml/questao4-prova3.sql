038b12a5c618db4564a1fd83e39cb606291a7fd4
João Matheus Villarim <joao.matheus.almeida@ccc.ufcg.edu.br>
2024-05-14 23:40:53 -0300

    Add questão que faltava em sql/biblioteca

--- sql/biblioteca/sql-dml/questao4-prova3.sql
+++ sql/biblioteca/sql-dml/questao4-prova3.sql
@@ -2,3 +2,12 @@
 Q4. Quais os usuários (código) que mais levaram livros?
 */
 
+SELECT u.codUsu
+FROM Usuario u, Emprestimo e
+WHERE u.codUsu = e.codUsu
+GROUP BY u.nome, u.endereco
+HAVING COUNT(*) >= ALL (
+  SELECT COUNT(*)
+  FROM Emprestimo e
+  GROUP BY e.codUsu
+);
